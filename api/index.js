3a428e44e058a89bb9b2215c20f8b4ecdb4737a1
mohamed enara <mohamedenara2003@gmail.com>
2025-09-13 03:35:47 +0300

    add new update

--- api/index.js
+++ api/index.js
@@ -1,15 +1,12 @@
-const serverless = require('serverless-http');
 const app = require('../app');
 const connectToDatabase = require('../utils/db');
 
 let isConnected = false;
 
-app.use(async (req, res, next) => {
+module.exports = async (req, res) => {
   if (!isConnected) {
     await connectToDatabase();
     isConnected = true;
   }
-  next();
-});
-
-module.exports = serverless(app);
+  app(req, res);
+};
